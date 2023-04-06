c7b54803dfab62cc1eabff539a96b0166745878c
kjohnson093 <kjohnson93@cub.uca.edu>
2023-04-06 18:19:34 -0500

    Added GameServer primary server functions, Added EventHandler to ServerGUI
    Deletd ServerGUIControl (replaced with EventHandler in GameServer)
    Also added stubs for important methods in GameServer.

--- team4UnoProject/gameServer/GameServer.java
+++ team4UnoProject/gameServer/GameServer.java
@@ -1,24 +1,38 @@
+/*
+ * Keondre Johnson
+ * 
+ * This program runs a server for handling clients connecting to each other in an UNO! game.
+ * The server will also maintain the database.
+ */
 package gameServer;
 
 import ocsf.server.*;
 import java.awt.Color;
 import java.io.IOException;
+import java.util.*;
+
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
 
-
+import database.*;
+import gameManagement.*;
+import playerGUI.*;
+import playerClient.*;
 
 public class GameServer extends AbstractServer {
 
 	//Corresponding text area and label with ServerGUI
 	private JTextArea log; 
 	private JLabel status; 
-	
+	private boolean running = false;
+	//private ArrayList<Game> activeGames;
+	private UserDatabase database;
+
 	public GameServer() {
 		super(12345); //dummy port to be overridden by input
 		setTimeout(500);
 	}
-	
+
 	//Set ServerGUI log and status
 	public void setLog(JTextArea log) {
 		this.log = log;
@@ -27,12 +41,161 @@ public class GameServer extends AbstractServer {
 		this.status = status;
 	}
 
-
-	public void handleMessageFromClient(Object arg0, ConnectionToClient arg1) {
-		// TODO Auto-generated method stub
-		
+	// Set database object to actual mySQL database
+	public void setDatabase(UserDatabase db) 
+	{
+		this.database = db;
 	}
-	
-	
+
+	// Getter that returns whether the server is currently running.
+	public boolean isRunning()
+	{
+		return running;
+	}
+
+	public void listeningException(Throwable exception) {
+		log.append("Listening Exception Occurred: " + exception.getMessage() + "\n");
+		exception.printStackTrace();;
+	}
+
+	//Update GUI in start event
+	public void serverStarted() {
+		log.append("Server Started" + "\n");
+		status.setText("Started");
+		status.setForeground(Color.GREEN);
+		running = true;
+	}
+
+	//Update GUI in stop event
+	public void serverStopped() {
+		status.setText("Stopped");
+		status.setForeground(Color.RED);
+		log.append("Server has stopped accepting new clients - Press Start to start accepting new clients." + "\n");
+		running = false;
+	}
+
+	//Update GUI in close event
+	public void serverClosed() {
+		//Update ServerGUI in close event
+		status.setText("Closed");
+		status.setForeground(Color.RED);
+		log.append("Server and all current clients closed - Press Start to restart." + "\n");
+		running = false;
+	}
+
+	//Update GUI when client connects
+	public void clientConnected(ConnectionToClient client) {
+		try {
+			client.sendToClient("username: client-" + client.getId());
+			log.append("Client " + client.getId() + " Connected" + "\n");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void handleMessageFromClient(Object arg0, ConnectionToClient arg1) 
+	{
+		// Check what kind of object arg0 is (sent from client), and serve it accordingly
+
+		//		// Handle login information
+		//		if (arg0 instanceof LoginData)
+		//		{
+		//			// Check the username and password with the database.
+		//			LoginData data = (LoginData)arg0;
+		//			Object result;
+		//			if (database.verifyAccount(data.getUsername(), data.getPassword()))
+		//			{
+		//				result = "LoginSuccessful";
+		//				log.append("Client " + arg1.getId() + " successfully logged in as " + data.getUsername() + "\n");
+		//			}
+		//			else
+		//			{
+		//				result = new Error("The username and password are incorrect.", "Login");
+		//				log.append("Client " + arg1.getId() + " failed to log in\n");
+		//			}
+		//
+		//			// Send the result to the client.
+		//			try
+		//			{
+		//				arg1.sendToClient(result);
+		//			}
+		//			catch (IOException e)
+		//			{
+		//				return;
+		//			}
+		//		}
+		//
+		//		// Handle create account
+		//		else if (arg0 instanceof CreateAccountData)
+		//		{
+		//			// Try to create the account.
+		//			CreateAccountData data = (CreateAccountData)arg0;
+		//			Object result;
+		//			if (database.createNewAccount(data.getUsername(), data.getPassword()))
+		//			{
+		//				result = "CreateAccountSuccessful";
+		//				log.append("Client " + arg1.getId() + " created a new account called " + data.getUsername() + "\n");
+		//			}
+		//			else
+		//			{
+		//				result = new Error("The username is already in use.", "CreateAccount");
+		//				log.append("Client " + arg1.getId() + " failed to create a new account\n");
+		//			}
+		//
+		//			// Send the result to the client.
+		//			try
+		//			{
+		//				arg1.sendToClient(result);
+		//			}
+		//			catch (IOException e)
+		//			{
+		//				return;
+		//			}
+		//
+		//		}
+		//
+		//		//Handle New Game
+		//		else if (arg0 instanceof NewGameData)
+		//		{
+		//			//Assign client player1
+		//			
+		//			//Wait for other players
+		//			
+		//			//Send to Game panel when full
+		//		}
+		//
+		//		//Handle Join Game
+		//		else if (arg0 instanceof JoinGameData)
+		//		{
+		//
+		//			//Search for existing Game slots
+		//			
+		//			//Assign player number
+		//			
+		//			// When game slot 2 (or 4) filled, create Game
+		//		}
+		//
+		//		//Handle Game object, validate moves, update game state and send to all clients
+		//		else if (arg0 instanceof GameData)
+		//		{
+		//			//Take in Game command
+		//			
+		//			//Based on command, manage the game by invoking its methods
+		//			
+		//			//Fetch existing  game
+		//				//Create initial hand and first cards
+		//			
+		//			//Get player turn 
+		//			
+		//			//Validate move
+		//			
+		//			//Update game
+		//			
+		//			
+		//			
+		//		}
+	}
+
 
 }
--- team4UnoProject/gameServer/ServerGUI.java
+++ team4UnoProject/gameServer/ServerGUI.java
@@ -3,9 +3,24 @@ package gameServer;
 import javax.swing.*;
 import java.awt.*;
 //import java.io.IOException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 public class ServerGUI extends JFrame {
 
+
+	private JButton start ;
+	private JButton close; 
+	private JButton stop; 
+	private JButton quit; 
+	private JLabel serverLogLabel;
+	private JLabel portLabel; 
+	private JTextField portField; 
+	private JLabel timeoutLabel;
+	private JTextField timeoutField;
+	private JTextArea serverLogTextArea; 
+	private GameServer server; 
 	//Constructor - Creates view and interactive components for controlling the server
 	public ServerGUI() {
 		setTitle("ServerGUI");
@@ -28,10 +43,10 @@ public class ServerGUI extends JFrame {
 		JPanel south = new JPanel();
 
 		//Server control buttons
-		JButton start = new JButton("Start");
-		JButton close = new JButton("Close");
-		JButton stop = new JButton("Stop");
-		JButton quit = new JButton("Quit");
+		start = new JButton("Start");
+		close = new JButton("Close");
+		stop = new JButton("Stop");
+		quit = new JButton("Quit");
 		south.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		south.add(start);
 		south.add(close);
@@ -44,33 +59,33 @@ public class ServerGUI extends JFrame {
 		center.setLayout(null);
 
 		//Server Log
-		JLabel serverLogLabel = new JLabel("Server Log Below");
+		serverLogLabel = new JLabel("Server Log Below");
 		serverLogLabel.setBounds(139, 91, 106, 22);
 		center.add(serverLogLabel);
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(0, 124, 386, 232);
 		center.add(scrollPane);
-		JTextArea serverLogTextArea = new JTextArea();
+		serverLogTextArea = new JTextArea();
 		scrollPane.setViewportView(serverLogTextArea);
 		serverLogTextArea.setLineWrap(true);
 		serverLogTextArea.setEditable(false);
 
 		//Port components
-		JLabel portLabel = new JLabel("Port #");
+		portLabel = new JLabel("Port #");
 		portLabel.setHorizontalAlignment(SwingConstants.RIGHT);
 		portLabel.setBounds(86, 11, 75, 17);
 		center.add(portLabel);
-		JTextField portField = new JTextField();
+		portField = new JTextField("8300");
 		portField.setBounds(171, 8, 96, 20);
 		center.add(portField);
 		portField.setColumns(10);
-		
+
 		//Timeout components
-		JLabel timeoutLabel = new JLabel("Timeout");
+		timeoutLabel = new JLabel("Timeout");
 		timeoutLabel.setHorizontalAlignment(SwingConstants.RIGHT);
 		timeoutLabel.setBounds(86, 42, 74, 17);
 		center.add(timeoutLabel);
-		JTextField timeoutField = new JTextField();
+		timeoutField = new JTextField("500");
 		timeoutField.setBounds(171, 39, 96, 20);
 		center.add(timeoutField);
 		timeoutField.setColumns(10);
@@ -83,8 +98,15 @@ public class ServerGUI extends JFrame {
 		//Set frame visible
 		setVisible(true);
 
+		//Event handling
+		EventHandler event = new EventHandler();
+		start.addActionListener(event);
+		close.addActionListener(event);
+		stop.addActionListener(event);
+		quit.addActionListener(event);
+
 		/////Create Server Object/////
-		GameServer server = new GameServer();
+		server = new GameServer();
 		//Set server log and status to GUI
 		server.setStatus(statusConditionLabel);
 		server.setLog(serverLogTextArea);
@@ -93,5 +115,89 @@ public class ServerGUI extends JFrame {
 
 	public static void main(String[] args) {
 		new ServerGUI();
+	}
+
+	// Class for handling events.
+	class EventHandler implements ActionListener
+	{
+		// Event handler for ActionEvent.
+		public void actionPerformed(ActionEvent e)
+		{
+			// Determine which button was clicked.
+			Object buttonClicked = e.getSource();
+
+			// Handle the Listen button.
+			if (buttonClicked == start)
+			{
+				// Display an error if the port number or timeout was not entered.
+				if (portField.getText().equals("") || timeoutField.getText().equals(""))
+				{
+					serverLogTextArea.append("Port number or timeout not entered before pressing Listen\n");
+				}
+
+				// Otherwise, tell the server to start listening with the user's settings.
+				else
+				{
+					int port = Integer.parseInt(portField.getText());
+					int timeout = Integer.parseInt(timeoutField.getText());
+					server.setPort(port);
+					server.setTimeout(timeout);
+					try
+					{
+						server.listen();
+						
+					}
+					catch (IOException e1)
+					{
+						serverLogTextArea.append("An exception occurred: " + e1.getMessage() + "\n");
+					}
+				}
+			}
+
+			// Handle the Close button.
+			else if (buttonClicked == close)
+			{
+				// Display an error if the server has not been started.
+				if (!server.isRunning())
+				{
+					serverLogTextArea.append("Server not currently started\n");
+				}
+
+				// Otherwise, close the server.
+				else
+				{
+					try
+					{
+						server.close();
+					}
+					catch (IOException e1)
+					{
+						serverLogTextArea.append("An exception occurred: " + e1.getMessage() + "\n");
+					}
+				}
+			}
+
+			// Handle the Stop button.
+			else if (buttonClicked == stop)
+			{
+				// Display an error if the server is not listening.
+				if (!server.isListening())
+				{
+					serverLogTextArea.append("Server not currently listening\n");
+				}
+
+				// Otherwise, stop listening.
+				else
+				{
+					server.stopListening();
+				}
+			}
+
+			// For the Quit button, just stop this program.
+			else if (buttonClicked == quit)
+			{
+				System.exit(0);
+			}
+		}
 	}
 }
