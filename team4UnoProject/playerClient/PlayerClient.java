922f542929f4656879a0c19c562aee0e0265627d
bricesimeu <77851864+bricesimeu@users.noreply.github.com>
2023-04-06 12:34:32 -0500

    Update PlayerClient.java

--- team4UnoProject/playerClient/PlayerClient.java
+++ team4UnoProject/playerClient/PlayerClient.java
@@ -1,5 +1,42 @@
 package playerClient;
 
-public class PlayerClient {
+//import ocsf.client.AbstractClient;
+
+public class PlayerClient extends AbstractClient 
+{
+
+//private CreateAccountControl createAccountControl;	
+//private  InitialControl  initialControl;
+//private  LoginControl loginControl;
+	
+//private LoginData loginData;
+//private NewGameData new
+	
+/*	
+ //Constructor
+public PlayerClient() {
+	super("localhost",8300);
+	}
+	
+
+public CreateAccountControl getCreateAccountControl() {
+	  return createAccountControl;
+	  }
+
+	
+public InitialControl getInitialControl() {
+		    return InitialControl;
+		  }
+public void setInitialControl(InitialControl initialControl) {
+		    InitialControl = initialControl;
+}
+	
+public LoginControl getLoginControl() {
+		    return loginControl;
+		  }
+		  public void setLoginControl(LoginControl loginControl) {
+		    this.loginControl = loginControl;
+		  }
+	*/
 
 }
