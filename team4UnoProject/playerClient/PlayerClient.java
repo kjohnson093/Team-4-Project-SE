c10399e7c3420e04f834286818eb83dcd96d408a
bricesimeu <77851864+bricesimeu@users.noreply.github.com>
2023-04-13 12:34:14 -0500

    Update PlayerClient.java

--- team4UnoProject/playerClient/PlayerClient.java
+++ team4UnoProject/playerClient/PlayerClient.java
@@ -5,17 +5,59 @@ import ocsf.client.AbstractClient;
 public class PlayerClient extends AbstractClient 
 {
 
-private CreateAccountControl createAccountControl;	
-private  InitialControl  initialControl;
-private  LoginControl loginControl;
+	private CreateAccountControl createAccountControl;	
+	private  InitialControl  initialControl;
+	private  LoginControl loginControl;
 	
-private LoginData loginData;
-private NewGameData new
-		
- //Constructor
-public PlayerClient() {
-	super("localhost",8300);
+	private LoginData loginData;
+	private NewGameData;
+	
+	public ChatClient()
+	{
+		super("local host", 8300);
 	}
+	
+		
+ @Override
+	protected void handleMessageFromServer(Object msg)
+	{
+		if(msg instanceof LoginData) {
+			LoginData ld = (LoginData)msg;
+			if(ld.getUsername().equals("")) {
+				System.out.println("Fail to login!");
+			}
+			else {
+				System.out.println("Login Successfully!");
+				lc.loginSuccess();
+			}
+		}
+		else if(msg instanceof CreateAccountData)
+		{
+			CreateAccountData cad = (CreateAccountData)msg;
+		if(cad.getUsername().equals("")) {
+			System.out.println("Fail to create new account!");
+			cac.displayError("Username already exist!");
+		}
+		else {
+			System.out.println("Account created successfully!");
+			cac.createSuccess();
+		}
+		}
+		else if(msg instanceof String) {
+			System.out.println("Server Message sent to Client\n" + msg);
+		}
+	}
+
+	public void connectionException (Throwable exception) {
+		System.out.println("Connection Exception Occurred");
+		System.out.println(exception.getMessage());
+		System.out.println(exception.getStackTrace());
+	}
+
+	public void connectionEstablished() {
+		System.out.println("Client Connected");
+	}
+}
 	
 
 public CreateAccountControl getCreateAccountControl() {
