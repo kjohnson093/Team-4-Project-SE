d02af315dd7020cbe43821931f85c01399580c7f
kjohnson093 <kjohnson93@cub.uca.edu>
2023-04-13 17:43:02 -0500

    Update GameServer login and create account

--- team4UnoProject/gameServer/GameServer.java
+++ team4UnoProject/gameServer/GameServer.java
@@ -98,103 +98,104 @@ public class GameServer extends AbstractServer {
 	{
 		// Check what kind of object arg0 is (sent from client), and serve it accordingly
 
-		//		// Handle login information
-		//		if (arg0 instanceof LoginData)
-		//		{
-		//			// Check the username and password with the database.
-		//			LoginData data = (LoginData)arg0;
-		//			Object result;
-		//			if (database.verifyAccount(data.getUsername(), data.getPassword()))
-		//			{
-		//				result = "LoginSuccessful";
-		//				log.append("Client " + arg1.getId() + " successfully logged in as " + data.getUsername() + "\n");
-		//			}
-		//			else
-		//			{
-		//				result = new Error("The username and password are incorrect.", "Login");
-		//				log.append("Client " + arg1.getId() + " failed to log in\n");
-		//			}
-		//
-		//			// Send the result to the client.
-		//			try
-		//			{
-		//				arg1.sendToClient(result);
-		//			}
-		//			catch (IOException e)
-		//			{
-		//				return;
-		//			}
-		//		}
-		//
-		//		// Handle create account
-		//		else if (arg0 instanceof CreateAccountData)
-		//		{
-		//			// Try to create the account.
-		//			CreateAccountData data = (CreateAccountData)arg0;
-		//			Object result;
-		//			if (database.createNewAccount(data.getUsername(), data.getPassword()))
-		//			{
-		//				result = "CreateAccountSuccessful";
-		//				log.append("Client " + arg1.getId() + " created a new account called " + data.getUsername() + "\n");
-		//			}
-		//			else
-		//			{
-		//				result = new Error("The username is already in use.", "CreateAccount");
-		//				log.append("Client " + arg1.getId() + " failed to create a new account\n");
-		//			}
-		//
-		//			// Send the result to the client.
-		//			try
-		//			{
-		//				arg1.sendToClient(result);
-		//			}
-		//			catch (IOException e)
-		//			{
-		//				return;
-		//			}
-		//
-		//		}
-		//
-		//		//Handle New Game
-		//		else if (arg0 instanceof NewGameData)
-		//		{
-		//			//Assign client player1
-		//			
-		//			//Wait for other players
-		//			
-		//			//Send to Game panel when full
-		//		}
-		//
-		//		//Handle Join Game
-		//		else if (arg0 instanceof JoinGameData)
-		//		{
-		//
-		//			//Search for existing Game slots
-		//			
-		//			//Assign player number
-		//			
-		//			// When game slot 2 (or 4) filled, create Game
-		//		}
-		//
-		//		//Handle Game object, validate moves, update game state and send to all clients
-		//		else if (arg0 instanceof GameData)
-		//		{
-		//			//Take in Game command
-		//			
-		//			//Based on command, manage the game by invoking its methods
-		//			
-		//			//Fetch existing  game
-		//				//Create initial hand and first cards
-		//			
-		//			//Get player turn 
-		//			
-		//			//Validate move
-		//			
-		//			//Update game
-		//			
-		//			
-		//			
-		//		}
+				// Handle login information
+				if (arg0 instanceof LoginData)
+				{
+					// Check the username and password with the database.
+					LoginData data = (LoginData)arg0;
+					Object result = "LoginFailed";
+					try {
+						if (database.verifyAccount(data.getUsername(), data.getPassword()))
+						{
+							result = "LoginSuccessful";
+							log.append("Client " + arg1.getId() + " successfully logged in as " + data.getUsername() + "\n");
+						}
+					} catch (Exception e1) {
+						// TODO Auto-generated catch block
+						log.append("Client " + arg1.getId() + " failed to log in\n");
+						e1.printStackTrace();
+					}
+		
+					// Send the result to the client.
+					try
+					{
+						arg1.sendToClient(result);
+					}
+					catch (IOException e)
+					{
+						return;
+					}
+				}
+		
+				// Handle create account
+				else if (arg0 instanceof CreateAccountData)
+				{
+					// Try to create the account.
+					CreateAccountData data = (CreateAccountData)arg0;
+					Object result = "CreateAccountFailed";
+					try {
+						if (database.createNewAccount(data.getUsername(), data.getPassword()))
+						{
+							result = "CreateAccountSuccessful";
+							log.append("Client " + arg1.getId() + " created a new account called " + data.getUsername() + "\n");
+						}
+					} catch (Exception e1) {
+						log.append("Client " + arg1.getId() + " failed to create a new account\n");
+						e1.printStackTrace();
+					}
+		
+					// Send the result to the client.
+					try
+					{
+						arg1.sendToClient(result);
+					}
+					catch (IOException e)
+					{
+						return;
+					}
+		
+				}
+		
+				//Handle New Game
+				else if (arg0 instanceof NewGameData)
+				{
+					//Assign client player1
+					
+					//Wait for other players
+					
+					//Send to Game panel when full
+				}
+		
+				//Handle Join Game
+				else if (arg0 instanceof JoinGameData)
+				{
+		
+					//Search for existing Game slots
+					
+					//Assign player number
+					
+					// When game slot 2 (or 4) filled, create Game
+				}
+		
+				//Handle Game object, validate moves, update game state and send to all clients
+				else if (arg0 instanceof GameData)
+				{
+					//Take in Game command
+					
+					//Based on command, manage the game by invoking its methods
+					
+					//Fetch existing  game
+						//Create initial hand and first cards
+					
+					//Get player turn 
+					
+					//Validate move
+					
+					//Update game
+					
+					
+					
+				}
 	}
 
 
