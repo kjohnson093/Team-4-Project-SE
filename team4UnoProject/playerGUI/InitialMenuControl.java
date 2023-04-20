da330a5d29bfd791a4e9dd6d454e22761522d02d
Julius Kwakye <77901590+joknny-git@users.noreply.github.com>
2023-04-19 22:29:12 -0500

    Update InitialMenuControl.java

--- team4UnoProject/playerGUI/InitialMenuControl.java
+++ team4UnoProject/playerGUI/InitialMenuControl.java
@@ -5,45 +5,49 @@ import javax.swing.*;
 
 import playerClient.PlayerClient;
 
+//import playerClient.PlayerClient;
+
 import java.awt.event.*;
 
 public class InitialMenuControl implements ActionListener
 {
-  // Private data field for storing the container.
-  private JPanel container;
-  private PlayerClient client;
-  // Constructor for the initial controller.
-  public InitialMenuControl(JPanel container, PlayerClient client)
-  {
-    this.container = container;
-    this.client = client;
-  }
- 
+	// Private data field for storing the container.
+	private JPanel container;
+	private PlayerClient client;
 
-// Handle button clicks.
-  public void actionPerformed(ActionEvent ae)
-  {
-    // Get the name of the button clicked.
-    String command = ae.getActionCommand();
-    
-    // The Login button takes the user to the login panel.
-    if (command.equals("Join Game"))
-    {
-      LoginPanel loginPanel = (LoginPanel)container.getComponent(1);
-      loginPanel.setError("");
-      CardLayout cardLayout = (CardLayout)container.getLayout();
-      cardLayout.show(container, "2");
-     
-    }
-    
-    // The Create button takes the user to the create account panel.
-    else if (command.equals("New Game"))
-    {
-      CreateAccountPanel createAccountPanel = (CreateAccountPanel)container.getComponent(2);
-      createAccountPanel.setError("");
-      CardLayout cardLayout = (CardLayout)container.getLayout();
-      cardLayout.show(container, "3");
-    }
-  }
+	// Constructor for the initial controller.
+	public InitialMenuControl(JPanel container, PlayerClient client)
+	{
+		this.container = container;
+		this.client = client;
+	}
+
+
+	// Handle button clicks.
+	public void actionPerformed(ActionEvent ae)
+	{
+		// Get the name of the button clicked.
+		String command = ae.getActionCommand();
+
+		// The Login button takes the user to the login panel.
+		if (command.equals("Join Game"))
+		{
+			GamePanel GamePanel = (GamePanel)container.getComponent(4);
+			//loginPanel.setError("");
+			CardLayout cardLayout = (CardLayout)container.getLayout();
+			cardLayout.show(container, "5");
+
+		}
+
+		// The Create button takes the user to the create account panel.
+		else if (command.equals("New Game"))
+		{
+			GamePanel gamePanel = (GamePanel)container.getComponent(4);
+			//createAccountPanel.setError("");
+			CardLayout cardLayout = (CardLayout)container.getLayout();
+			cardLayout.show(container, "5");
+		}
+	}
 }
+
 
