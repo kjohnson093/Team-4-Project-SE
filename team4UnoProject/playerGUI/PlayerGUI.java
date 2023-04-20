06e36498516e0ea078554e5775cf84f3b92cd578
Julius Kwakye <77901590+joknny-git@users.noreply.github.com>
2023-04-19 22:39:00 -0500

    Update PlayerGUI.java

--- team4UnoProject/playerGUI/PlayerGUI.java
+++ team4UnoProject/playerGUI/PlayerGUI.java
@@ -8,12 +8,11 @@ import java.awt.*;
 import java.io.IOException;
 
 public class PlayerGUI extends JFrame
-{
-  
-  
+{  
   // Constructor that creates the client GUI.
   public PlayerGUI()
   {
+	  
     // Set up the chat client.
 	  PlayerClient client = new PlayerClient();
     client.setHost("localhost");
@@ -28,11 +27,10 @@ public class PlayerGUI extends JFrame
     }
     
     
-    
     // Set the title and default close operation.
-    this.setTitle("Player");
+    this.setTitle("UNO");
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    getContentPane().setBackground(Color.RED);    
+    //getContentPane().setBackground(Color.RED);    
     // Create the card layout container.
     CardLayout cardLayout = new CardLayout();
     JPanel container = new JPanel(cardLayout);
@@ -44,22 +42,27 @@ public class PlayerGUI extends JFrame
     LoginControl lc = new LoginControl(container,client);
     CreateAccountControl cac = new CreateAccountControl(container,client);
     InitialMenuControl imc = new InitialMenuControl(container, client);
+    GameControl gc = new GameControl(container, client);
+    
     //Set the client info
     client.setLoginControl(lc);
     client.setCreateAccountControl(cac);
     client.setInitialMenuControl(imc);
+    client.setGameControl(gc);
     
     // Create the four views. (need the controller to register with the Panels
     JPanel view1 = new InitialPanel(ic);
     JPanel view2 = new LoginPanel(lc);
     JPanel view3 = new CreateAccountPanel(cac);
     JPanel view4 = new InitialMenuPanel(imc);
+    JPanel view5 = new GamePanel(gc);
     
     // Add the views to the card layout container.
     container.add(view1, "1");
     container.add(view2, "2");
     container.add(view3, "3");
     container.add(view4, "4");
+    container.add(view5, "5");
    
     
     // Show the initial view in the card layout.
@@ -67,7 +70,7 @@ public class PlayerGUI extends JFrame
     
     // Add the card layout container to the JFrame.
     // GridBagLayout makes the container stay centered in the window.
-    this.setLayout(new GridBagLayout());
+    //this.setLayout(new GridBagLayout());
     this.add(container);
 
     // Show the JFrame.
@@ -75,6 +78,7 @@ public class PlayerGUI extends JFrame
     this.setVisible(true);
   }
 
+  
   // Main function that creates the client GUI when the program is started.
   public static void main(String[] args)
   {
